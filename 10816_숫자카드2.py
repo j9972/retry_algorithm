2d118392dc34ff45bb3daec90ee7f28ef250a318
j9972 <j9972@naver.com>
2025-12-28 16:47:52 +0900

    counter 사용해서 시간 단축

--- 10816_숫자카드2.py
+++ 10816_숫자카드2.py
@@ -1,4 +1,5 @@
 import sys
+from collections import Counter
 input = sys.stdin.readline
 
 MAX_CNT = 10000000
@@ -6,16 +7,20 @@ MAX_CNT = 10000000
 n = int(input())
 n_list = list(map(int,input().split()))
 
+counter = Counter(n_list)
+
 m = int(input())
 m_list = list(map(int,input().split()))
 
-arr = [0 for _ in range(MAX_CNT*2+1)]
+print(' '.join(str(counter[i]) for i in m_list))
 
-for i in n_list:
-    arr[i+MAX_CNT] += 1
+# arr = [0 for _ in range(MAX_CNT*2+1)]
 
-for i in m_list:
-    print(arr[i+MAX_CNT], end=' ')
-print()
+# for i in n_list:
+#     arr[i+MAX_CNT] += 1
+
+# for i in m_list:
+#     print(arr[i+MAX_CNT], end=' ')
+# print()
 
 
