cc930d9b4f0796f01e3d1de61ef2797f920adb0d
j9972 <j9972@naver.com>
2025-12-29 18:41:39 +0900

    666에 숫자를 붙이는게 아니라 666부터 1씩 증가하면서 그 수에 666이 있으면 ?번째 종말이다 라는 식으로 문제를 이해하기

---
+++ 1436_영화감독.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+num, cnt = 666, 0
+
+while True:
+    if '666' in str(num):
+        cnt += 1
+    if cnt == n:
+        print(num)
+        break
+    num += 1
