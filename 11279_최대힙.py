89467474b36f172d03ad3e3855364a08c8ed07ba
j9972 <j9972@naver.com>
2026-01-12 23:46:52 +0900

    최대힙은 push , pop 모두 - 부호를 달아주기

---
+++ 11279_최대힙.py
@@ -0,0 +1,20 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+heap = []
+
+
+n = int(input())
+
+for _ in range(n):
+    val = int(input())
+
+    if val == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, -1 * val)
+    
