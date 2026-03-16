34d709ced9d7462248ec6fded3fb9f9ccf762011
j9972 <j9972@naver.com>
2026-03-16 22:27:14 +0900

    retry - reverse_flag

--- 5430_AC.py
+++ 5430_AC.py
@@ -7,9 +7,9 @@ for tc in range(int(input())):
 
     p = input()
     n = int(input())
-    arr = input().rstrip()[1:-1]  
+    arr = input().rstrip()[1:-1]
 
-    if n == 0 or arr == '':
+    if len(arr) == 0 or n == 0:
         new_arr = deque()
     else:
         new_arr = deque(map(int, arr.split(',')))
@@ -17,23 +17,26 @@ for tc in range(int(input())):
     reverse_flag, flag = False, False
 
     for char in p:
+
         if char == 'R':
             reverse_flag = not reverse_flag
+
         elif char == 'D':
+
             if len(new_arr) == 0:
                 flag = True
-                break
-                
-            
-            if reverse_flag:
-                new_arr.pop()
             else:
-                new_arr.popleft()
+
+                if reverse_flag:
+                    new_arr.pop()
+                else:
+                    new_arr.popleft()
     
     if flag:
         print('error')
     else:
         if reverse_flag:
-            print('[' + ','.join(map(str, reversed(new_arr))) + ']')
+            print('['+','.join(map(str,reversed(new_arr)))+']')
         else:
-            print('[' + ','.join(map(str, new_arr)) + ']')
+            print('['+','.join(map(str,new_arr))+']')
+
